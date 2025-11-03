cfae841d6df1b51f3ce8e6323ce975dc88cfa93d
non0ky <153778567+non0ky@users.noreply.github.com>
2025-11-02 22:32:00 -0300

    cinco nuevas imagenes de ruido blanco

--- javascript/random_image_scroll.js
+++ javascript/random_image_scroll.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
         '../data/ruido_blanco/t-2.png',
         '../data/ruido_blanco/t-3.png',
         '../data/ruido_blanco/t-4.png',
+        '../data/ruido_blanco/t-5.png',
+        '../data/ruido_blanco/t-6.png',
+        '../data/ruido_blanco/t-7.png',
+        '../data/ruido_blanco/t-8.png',
+        '../data/ruido_blanco/t-9.png',
     ];
     const randomIndex = Math.floor(Math.random() * versiones.length);
     const imagenAleatoria = versiones[randomIndex];
