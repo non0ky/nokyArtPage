3746348022016136c7f10de537764f3f74860029
non0ky <153778567+non0ky@users.noreply.github.com>
2025-11-05 19:21:25 -0300

    boton invisible e imprimir el array de nodos

--- javaScript/index_diagram.js
+++ javaScript/index_diagram.js
@@ -1,88 +1,86 @@
 var nodes = new vis.DataSet([
-    { id: 1, label: 'conexiones tecno-humanas\n(nexo)', x: -28, y: 27 }, // (eve)
-    { id: 2, label: 'entidades perifericas\n(nexo)', x: 80, y: -46 },  // (awa)
-
-    { id: 3, label: 'mind-to-node\n(2024)', x: 23, y: 158 },   // (iwi)
-    { id: 4, label: 'internal coding\n(2023)', x: 11, y: -110 }, // (owo)
-    { id: 5, label: 'dont cry cam\n(2022)', x: -70, y: -188 }, // (twt)
-    { id: 6, label: 'la televisión de mi abuelita\n(tesis)\n(2024)', x: -139, y: 113 },
-
-    { id: 7, label: 'trapped-man\n(2024)', x: -127, y: -76 }, // (1o1)
-    { id: 8, label: 'via del tren (quilicura)\n(2023)', x: 206, y: 13 }, // (0o0)
-    { id: 9, label: 'ruido blanco\n(2025)', x: 206, y: 13 },
+  { id: 1, label: 'conexiones tecno-humanas\n(nexo)', x: -9, y: 38 },
+  { id: 2, label: 'entidades perifericas\n(nexo)', x: 130, y: -24 },
+  { id: 3, label: 'mind-to-node\n(2024)', x: -88, y: 156 },
+  { id: 4, label: 'internal coding\n(2023)', x: -49, y: -102 },
+  { id: 5, label: 'dont cry cam\n(2022)', x: -170, y: -133 },
+  { id: 6, label: 'la televisión de mi abuelita\n(tesis)\n(2024)', x: 66, y: 160 },
+  { id: 7, label: 'trapped-man\n(2024)', x: -148, y: -11 },
+  { id: 8, label: 'via del tren (quilicura)\n(2023)', x: 154, y: -156 },
+  { id: 9, label: 'ruido blanco\n(2025)', x: 250, y: 36 },
 ]);
 
 let links = [
-    "../html/conexiones_tecnohumanas.html", // (twt)
-    "../html/entidades_perifericas.html", // 8.8
+  "../html/conexiones_tecnohumanas.html", // (twt)
+  "../html/entidades_perifericas.html", // 8.8
 
-    "../html/nodes.html", // (i.i)
-    "../html/internal_coding.html", // (qvq)
-    "../html/dontcrycam.html", // (. .)
-    "../html/television_abuelita.html", // o,,,,o
+  "../html/nodes.html", // (i.i)
+  "../html/internal_coding.html", // (qvq)
+  "../html/dontcrycam.html", // (. .)
+  "../html/television_abuelita.html", // o,,,,o
 
-    "../html/trapped_man.html", // (.-.)
-    "../html/new_linea_tren.html", // (-_-)
-    "../html/ruido_blanco.html",
+  "../html/trapped_man.html", // (.-.)
+  "../html/new_linea_tren.html", // (-_-)
+  "../html/ruido_blanco.html",
 ];
 
 var edges = new vis.DataSet([
-    { from: 1, to: 3 },
-    { from: 5, to: 4 },
-    { from: 4, to: 1 },
-    
-    { from: 1, to: 6 },
-    { from: 5, to: 7 },
-    { from: 2, to: 8 },
-    { from: 2, to: 1 },
-    { from: 7, to: 1 },
-    { from: 2, to: 9 },
+  { from: 1, to: 3 },
+  { from: 5, to: 4 },
+  { from: 4, to: 1 },
+
+  { from: 1, to: 6 },
+  { from: 5, to: 7 },
+  { from: 2, to: 8 },
+  { from: 2, to: 1 },
+  { from: 7, to: 1 },
+  { from: 2, to: 9 },
 ]);
 
 var container = document.getElementById('network');
 var data = {
-    nodes: nodes,
-    edges: edges
+  nodes: nodes,
+  edges: edges
 };
 
 var options = {
-    nodes: {
-        chosen: false,
-        shape: 'box',
-        color: {
-            background: 'black',
-            border: 'white',
-        },
-        font: {
-            color: 'white'
-        },
-        size: 15
+  nodes: {
+    chosen: false,
+    shape: 'box',
+    color: {
+      background: 'black',
+      border: 'white',
     },
-    edges: {
-        color: 'white',
-        arrows: {
-            to: { enabled: true, scaleFactor: 0.5 }
-        },
+    font: {
+      color: 'white'
     },
-    interaction: {
-        hover: true
+    size: 15
+  },
+  edges: {
+    color: 'white',
+    arrows: {
+      to: { enabled: true, scaleFactor: 0.5 }
     },
-    layout: {
-        improvedLayout: false
-    }
+  },
+  interaction: {
+    hover: true
+  },
+  layout: {
+    improvedLayout: false
+  }
 };
 
 var network = new vis.Network(container, data, options);
 
 network.on("click", function (params) {
-    if (params.nodes.length > 0) {
-        var nodeId = params.nodes[0];
-        handleNodeClick(nodeId);
-    }
+  if (params.nodes.length > 0) {
+    var nodeId = params.nodes[0];
+    handleNodeClick(nodeId);
+  }
 });
 
 function handleNodeClick(nodeId) {
-    window.location.href = links[nodeId - 1];
+  window.location.href = links[nodeId - 1];
 }
 
 let blinkNodeId = null;
@@ -90,7 +88,7 @@ let blinkInterval = null;
 let blinkToggle = false;
 let originalColors = {};
 
-network.on("hoverNode", function(params) {
+network.on("hoverNode", function (params) {
   const nodeId = params.node;
   blinkNodeId = nodeId;
 
@@ -113,7 +111,7 @@ network.on("hoverNode", function(params) {
   }, 10);
 });
 
-network.on("blurNode", function(params) {
+network.on("blurNode", function (params) {
   const nodeId = params.node;
   clearInterval(blinkInterval);
   blinkInterval = null;
@@ -136,5 +134,15 @@ network.on("blurNode", function(params) {
 
 function guardarPosiciones() {
   var positions = network.getPositions();
-  console.log(JSON.stringify(positions));
+  var allNodes = nodes.get();
+
+  var output = "";
+  allNodes.forEach(function (node) {
+    var pos = positions[node.id];
+    var fixedLabel = node.label ? node.label.replace(/\n/g, '\\n') : '';
+
+    output += `{ id: ${node.id}, label: '${fixedLabel}', x: ${Math.round(pos.x)}, y: ${Math.round(pos.y)} },\n`;
+  });
+
+  console.log(output);
 }
